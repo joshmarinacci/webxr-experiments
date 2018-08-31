8291e3574071826384c88ad0c3f4adc5823c6185
Josh Marinacci <joshua@marinacci.org>
2018-08-31 15:18:38 -0700

    more work on the game

--- nonogram/nonogram.js
+++ nonogram/nonogram.js
@@ -6,9 +6,12 @@ export const STATES = {
 
 export default class Nonogram {
     constructor() {
-        this.data = [[0,1,0],
-                     [1,0,1],
-                     [0,1,1]]
+        this.data = [
+            [0,1,0,0],
+            [1,0,1,0],
+            [0,1,1,0],
+            [0,1,1,0],
+        ]
         this.width = this.data[0].length
         this.height = this.data.length
 
@@ -16,6 +19,7 @@ export default class Nonogram {
         for(let j=0; j<this.height; j++) {
             this.state[j] = []
             for(let i=0; i<this.width; i++) {
+                // this.state[j][i] = this.data[j][i]
                 this.state[j][i] = STATES.UNKNOWN
             }
         }
@@ -40,9 +44,48 @@ export default class Nonogram {
             for(let i=0; i<this.width; i++) {
                 const ans = this.data[j][i]
                 const cur = this.state[j][i]
-                if(ans === cur) correctCount++
+                if(ans === 1 && cur === 1) correctCount++
+                if(ans === 0 && (cur === 0 || cur === 2)) correctCount++
             }
         }
         return correctCount
+    }
+
+    checkFinished() {
+        return this.checkValid() === this.width * this.height
+    }
+
+
+    calculateColumnCounts(col) {
+        const counts = []
+        let count = 0;
+        let prev = 0
+        for(let j=0; j<this.height; j++) {
+            const val = this.data[j][col]
+            if(val === 1) count++
+            if(val === 0 && val !== prev) {
+                counts.push(count)
+                count = 0
+            }
+            prev = val
+        }
+        if(count > 0) counts.push(count)
+        return counts
+    }
+    calculateRowCounts(row) {
+        const counts = []
+        let count = 0;
+        let prev = 0
+        for(let j=0; j<this.width; j++) {
+            const val = this.data[row][j]
+            if(val === 1) count++
+            if(val === 0 && val !== prev) {
+                counts.push(count)
+                count = 0
+            }
+            prev = val
+        }
+        if(count > 0) counts.push(count)
+        return counts
     }
 }
