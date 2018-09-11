52564e3bdbb5810b70de59a371867b837948159b
Josh Marinacci <joshua@marinacci.org>
2018-09-11 10:48:27 -0700

    remove some dead code consolidate click code between mouse and controller versions

--- nonogram/pointer.js
+++ nonogram/pointer.js
@@ -9,14 +9,12 @@ export default class Pointer {
         this.waitcb = null
 
 
+
+        // setup the mouse
         this.canvas.addEventListener('mousemove', this.cameraFollowMouse.bind(this))
         this.canvas.addEventListener('click', this.mouseClick.bind(this))
 
-        // controllers
-
-        // const rc = new THREE.Raycaster()
-
-
+        // setup the VR controllers
         this.controller1 = this.renderer.vr.getController(0);
         this.controller1.addEventListener('selectstart', this.controllerSelectStart.bind(this));
         this.controller1.addEventListener('selectend', this.controllerSelectEnd.bind(this));
@@ -43,25 +41,8 @@ export default class Pointer {
         this.controller2.add(new THREE.Line(geometry, material));
     }
 
+    //override this to do something w/ the controllers on every tick
     tick(time) {
-        // function handleController( controller ) {
-        //     if ( controller.userData.isSelecting ) {
-        /*
-        var object = room.children[ 0 ];
-        room.remove( object );
-        object.position.copy( controller.position );
-        object.userData.velocity.x = ( Math.random() - 0.5 ) * 0.02;
-        object.userData.velocity.y = ( Math.random() - 0.5 ) * 0.02;
-        object.userData.velocity.z = ( Math.random() * 0.02 - 0.1 );
-        object.userData.velocity.applyQuaternion( controller.quaternion );
-        room.add( object );
-        */
-        // }
-        // }
-
-        // handleController( this.controller1 );
-        // handleController( this.controller2 );
-
     }
 
 
@@ -71,6 +52,7 @@ export default class Pointer {
         obj.listeners[type].forEach(cb => cb(payload))
     }
 
+    //make the camera follow the mouse in desktop mode. Helps w/ debugging.
     cameraFollowMouse(e) {
         const bounds = this.canvas.getBoundingClientRect()
         const ry = ((e.clientX - bounds.left) / bounds.width) * 2 - 1
@@ -79,17 +61,13 @@ export default class Pointer {
         this.camera.rotation.x = +rx
     }
 
-    mouseClick(e) {
+    _processClick() {
         if (this.waitcb) {
             this.waitcb()
             this.waitcb = null
             return
         }
-        const mouse = new THREE.Vector2()
-        const bounds = this.canvas.getBoundingClientRect()
-        mouse.x = ((e.clientX - bounds.left) / bounds.width) * 2 - 1
-        mouse.y = -((e.clientY - bounds.top) / bounds.height) * 2 + 1
-        this.raycaster.setFromCamera(mouse, this.camera)
+
         const intersects = this.raycaster.intersectObjects(this.scene.children, true)
         let fired = false
         intersects.forEach((it) => {
@@ -99,37 +77,26 @@ export default class Pointer {
             // }
         })
     }
+    mouseClick(e) {
+        const mouse = new THREE.Vector2()
+        const bounds = this.canvas.getBoundingClientRect()
+        mouse.x = ((e.clientX - bounds.left) / bounds.width) * 2 - 1
+        mouse.y = -((e.clientY - bounds.top) / bounds.height) * 2 + 1
+        this.raycaster.setFromCamera(mouse, this.camera)
+        this._processClick()
+    }
 
     controllerSelectStart(e) {
         e.target.userData.isSelecting = true;
-
-        if (this.waitcb) {
-            this.waitcb()
-            this.waitcb = null
-            return
-        }
-
-
-        const c = e.target
-        const origin = c.position
-        const dir = new THREE.Vector3(0, 0, -1)
-        dir.applyQuaternion(c.quaternion)
-        this.raycaster.set(c.position, dir)
-
-        const intersects = this.raycaster.intersectObjects(this.scene.children, true)
-        let fired = false
-        intersects.forEach((it) => {
-            // if(!fired && it.object.jtype) {
-            console.log(it)
-            this.fire(it.object, 'click', {type: 'click'})
-            fired = true
-            // }
-        })
-
     }
 
     controllerSelectEnd(e) {
         e.target.userData.isSelecting = false;
+        const c = e.target
+        const dir = new THREE.Vector3(0, 0, -1)
+        dir.applyQuaternion(c.quaternion)
+        this.raycaster.set(c.position, dir)
+        this._processClick()
     }
 
     waitSceneClick(cb) {
