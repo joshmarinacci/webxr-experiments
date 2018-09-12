a19702eaa517dfe65c39220c01716dc4a1e83253
Josh Marinacci <joshua@marinacci.org>
2018-09-12 13:23:13 -0700

    add a simple timer class setup overlay text only once instead of on every level create constants for the cells to reuse the materials and geometry

---
+++ nonogram/jtimer.js
@@ -0,0 +1,18 @@
+export default class JTimer {
+    constructor() {
+        this.active = {}
+    }
+
+    start(str) {
+        this.startTime = performance.now()
+
+        this.active[str] = performance.now()
+    }
+
+    stop(str) {
+        if(!this.active[str]) return
+        const dur = performance.now() - this.active[str]
+        this.active[str] = null
+        console.log(str + " " + dur)
+    }
+}
