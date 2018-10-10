ecee995998a9965d7809d5137c9b154dcd7200e0
Josh Marinacci <joshua@marinacci.org>
2018-10-10 16:18:29 -0700

    update scratch code

--- scratch.js
+++ scratch.js
@@ -1,3 +1,4 @@
+//constants for colors
 const BLACK = 0x000000;
 const WHITE = 0xFFFFFF;
 const BLUE = 0x0000FF;
@@ -5,7 +6,7 @@ const RED = 0xFF0000;
 
 
 function drawFrame(frame) {
-    //black columns
+    //black and white columns
     for(let y = 0; y < frame.height; y++) {
         for(let x=0; x<frame.width; x++) {
             //even or odd
@@ -18,12 +19,18 @@ function drawFrame(frame) {
         }
     }
 
+    //calculate the smaller of width or height
+    const size = Math.min(frame.width,frame.height)
 
-    frame.color = BLUE
-    frame.drawLine(0,0,frame.width,frame.height)
-
+    //fill the corner with a red square
     frame.color = RED
-    frame.fillRect(0,0,frame.width/2, frame.height/2)
+    frame.fillRect(0,0,size/2, size/2)
+
+    //diagonal blue lines for an X
+    frame.color = 0x0000FF;
+    frame.drawLine(0,0,size,size)
+    frame.drawLine(size-1,0,0,size)
+
 }
 
 class Frame {
@@ -65,14 +72,31 @@ class Frame {
             }
         }
     }
+    drawLine(x0,y0, x1,y1) {
+        //Bresenham's from Rosetta Code
+        // http://rosettacode.org/wiki/Bitmap/Bresenham's_line_algorithm#JavaScript
+        x0 = Math.floor(x0);
+        y0 = Math.floor(y0);
+        x1 = Math.floor(x1);
+        y1 = Math.floor(y1);
+        const dx = Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+        const dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1
+        let err = (dx>dy ? dx : -dy)/2;
+
+        while (true) {
+            this.setPixel(x0,y0);
+            if (x0 === x1 && y0 === y1) break;
+            const e2 = err
+            if (e2 > -dx) { err -= dy; x0 += sx; }
+            if (e2 < dy) { err += dx; y0 += sy; }
+        }
+    }
+
 }
 
 
 export function transform(buffer, rows, cols, frameCount, fps, isFirst) {
-    for (let i = 0; i < frameCount; i++) {
-        const frame = new Frame(buffer, rows, cols, i)
-        drawFrame(frame)
-    }
+    for (let i = 0; i < frameCount; i++) drawFrame(new Frame(buffer, rows, cols, i))
 }
 
 export default function () {
