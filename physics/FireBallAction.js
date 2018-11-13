c828a3b42ee1ba44f2ef2e615a071289fa4270f8
Josh Marinacci <joshua@marinacci.org>
2018-11-13 15:08:17 -0800

    move fireball action to separate class

---
+++ physics/FireBallAction.js
@@ -0,0 +1,30 @@
+export default class FireBallAction {
+    constructor(scene, controller, blockService) {
+        this.clock = new THREE.Clock()
+        this.press_sphere = null
+        this.startPressSphere = () => {
+            this.clock.start()
+            this.press_sphere = new THREE.Mesh(
+                new THREE.SphereGeometry(0.2),
+                new THREE.MeshLambertMaterial({color: 'red'})
+            )
+            this.press_sphere.scale.set(0.1, 0.1, 0.1)
+            this.press_sphere.position.set(0, 2, -2)
+            scene.add(this.press_sphere)
+        }
+        this.endPressSphere = (e) => {
+            scene.remove(this.press_sphere)
+            const strength = Math.min(1.0, this.clock.getElapsedTime() / 2.0)
+            this.clock.stop()
+            const ball = blockService.fireBall(controller.position, e.point.clone(), strength)
+            //remove the ball after 3 sec
+            setTimeout(() => blockService.removeBall(ball), 3 * 1000)
+        }
+    }
+    updatePressSphere(time) {
+        if (this.press_sphere) {
+            const strength = Math.min(1.0, this.clock.getElapsedTime() / 2.0)
+            this.press_sphere.scale.set(strength, strength, strength)
+        }
+    }
+}
