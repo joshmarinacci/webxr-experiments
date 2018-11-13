e7d8f8484fd5e4be69b26851ec3b42383a79d689
Josh Marinacci <joshua@marinacci.org>
2018-11-13 12:04:11 -0800

    move BlockSelection to a new file

---
+++ physics/BlockSelection.js
@@ -0,0 +1,66 @@
+const PROP_NAMES = ['x','y','z','rotx','roty','rotz','w','h','d']
+
+export default class BlockSelection {
+    constructor() {
+        this.blocks = []
+        this.listeners = {}
+    }
+    on(type,cb) {
+        this.addEventListener(type,cb)
+    }
+    addEventListener(type,cb) {
+        if(!this.listeners[type]) this.listeners[type] = []
+        this.listeners[type].push(cb)
+    }
+    fire(type) {
+        if(!this.listeners[type]) this.listeners[type] = []
+        this.listeners[type].forEach(cb => cb())
+    }
+    clear() {
+        this.blocks.forEach(obj=>obj.unselectSelf())
+        this.blocks = []
+        this.fire('changed')
+    }
+    add(obj) {
+        this.blocks.push(obj)
+        obj.selectSelf()
+        this.fire('changed')
+    }
+    addAll(arr) {
+        arr.forEach(b => this.add(b))
+    }
+    getBlocks() {
+        return this.blocks
+    }
+    decrementProperty(name) {
+        this.blocks.forEach(b => {
+            if(PROP_NAMES.indexOf(name) >= 0) {
+                b.set(name, b.get(name)-0.1)
+                return
+            }
+        })
+        this.fire('changed')
+    }
+    incrementProperty(name) {
+        this.blocks.forEach(b => {
+            if(PROP_NAMES.indexOf(name) >= 0) {
+                b.set(name, b.get(name)+0.1)
+                return
+            }
+        })
+        this.fire('changed')
+    }
+    getProperty(name) {
+        if(this.blocks.length <= 0) return -1
+        const block = this.blocks[0]
+        if(PROP_NAMES.indexOf(name) >= 0) {
+            return block.get(name)
+        }
+        return -1
+    }
+    setPropertyValue(name,value) {
+        this.blocks.forEach(b => {
+            b.set(name,value)
+        })
+    }
+}
