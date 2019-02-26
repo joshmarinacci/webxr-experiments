771369963f4dea293d88c45c10f72b4405ee059a
Josh Marinacci <joshua@marinacci.org>
2019-02-26 15:42:57 -0800

    first version of breakout 3d

---
+++ canvas2vr/WebXRBoilerPlate.js
@@ -0,0 +1,100 @@
+import {Mesh, BoxBufferGeometry,
+    MeshLambertMaterial, Color, DirectionalLight,
+    Scene,
+    PerspectiveCamera,
+    WebGLRenderer,
+    DefaultLoadingManager,
+} from "./node_modules/three/build/three.module.js"
+
+import VRManager, {VR_DETECTED} from "./vrmanager.js";
+
+export default class WebXRBoilerPlate {
+    constructor(options) {
+        this.listeners = {}
+        this.container = options.container
+        this.resizeOnNextRepaint = false
+    }
+    addEventListener(type,cb) {
+        if(!this.listeners[type]) this.listeners[type] = []
+        this.listeners[type].push(cb)
+    }
+
+    init() {
+        //create DIV for the canvas
+        // const container = document.createElement( 'div' );
+        // document.body.appendChild( container );
+        this.scene = new Scene();
+        console.log('container size', this.container.clientWidth)
+        this.camera = new PerspectiveCamera(70, this.container.clientWidth / this.container.clientHeight, 0.1, 50);
+        this.renderer = new WebGLRenderer({antialias: true});
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+        this.renderer.gammaOutput = true
+        this.renderer.vr.enabled = true;
+        this.container.appendChild(this.renderer.domElement);
+        this.vrmanager = new VRManager(this.renderer)
+        this.vrmanager.addEventListener(VR_DETECTED,()=>{
+            if(this.listeners.detected) this.listeners.detected.forEach(cb => cb(this))
+        })
+        // document.body.appendChild( WEBVR.createButton( renderer ) );
+
+        // initContent(scene,camera,renderer)
+
+        DefaultLoadingManager.onStart = (url, loaded, total) => {
+            console.log(`loading ${url}.  loaded ${loaded} of ${total}`)
+        }
+        DefaultLoadingManager.onLoad = () => {
+            console.log(`loading complete`)
+            // $("#loading-indicator").style.display = 'none'
+            // $("#enter-button").style.display = 'block'
+            // $("#enter-button").removeAttribute('disabled')
+            if (this.listeners.loaded) this.listeners.loaded.forEach(cb => cb(this))
+        }
+        DefaultLoadingManager.onProgress = (url, loaded, total) => {
+            console.log(`prog ${url}.  loaded ${loaded} of ${total}`)
+            if(this.listeners.progress) this.listeners.progress.forEach(cb => cb(loaded/total))
+            // $("#progress").setAttribute('value',100*(loaded/total))
+        }
+        DefaultLoadingManager.onError = (url) => {
+            console.log(`error loading ${url}`)
+        }
+
+        this.lastSize = { width: 0, height: 0}
+        this.render = () => {
+            if (this.onRenderCb) this.onRenderCb(this)
+            this.checkContainerSize()
+            this.renderer.render(this.scene, this.camera);
+        }
+
+        this.renderer.setAnimationLoop(this.render)
+
+        return new Promise((res, rej) => {
+            res(this)
+        })
+    }
+
+
+    onRender(cb) {
+        this.onRenderCb = cb
+    }
+
+    enterVR() {
+        this.vrmanager.enterVR()
+    }
+
+    playFullscreen() {
+        console.log("entering full screen")
+        this.resizeOnNextRepaint = true
+        this.container.requestFullscreen()
+    }
+
+    checkContainerSize() {
+        if(this.lastSize.width !== this.container.clientWidth || this.lastSize.height !== this.container.clientHeight) {
+            this.lastSize.width = this.container.clientWidth
+            this.lastSize.height = this.container.clientHeight
+            this.camera.aspect = this.lastSize.width / this.lastSize.height;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(this.lastSize.width, this.lastSize.height);
+        }
+    }
+}
---
+++ canvas2vr/breakout.js
@@ -0,0 +1,192 @@
+export default class Game {
+    constructor(canvas) {
+        this.canvas = canvas
+
+
+        var ctx = canvas.getContext("2d");
+        var ballRadius = 10;
+        var x = canvas.width/2;
+        var y = canvas.height-30;
+        var dx = 2;
+        var dy = -2;
+        var paddleHeight = 10;
+        var paddleWidth = 75;
+        var paddleX = (canvas.width-paddleWidth)/2;
+        var rightPressed = false;
+        var leftPressed = false;
+        var brickRowCount = 5;
+        var brickColumnCount = 3;
+        var brickWidth = 75;
+        var brickHeight = 20;
+        var brickPadding = 10;
+        var brickOffsetTop = 30;
+        var brickOffsetLeft = 30;
+        var score = 0;
+        var lives = 3;
+
+        const bricks = []
+        for(let c=0; c<brickColumnCount; c++) {
+            bricks[c] = [];
+            for(let r=0; r<brickRowCount; r++) {
+                bricks[c][r] = { x: 0, y: 0, status: 1 };
+            }
+        }
+
+        this.keyDownHandler = (e) => {
+            if(e.keyCode === 39) {
+                rightPressed = true;
+            }
+            else if(e.keyCode === 37) {
+                leftPressed = true;
+            }
+        }
+        this.keyUpHandler = (e) => {
+            if(e.keyCode === 39) {
+                rightPressed = false;
+            }
+            else if(e.keyCode === 37) {
+                leftPressed = false;
+            }
+        }
+        this.mouseMoveHandler = (e) => {
+            const relativeX = e.clientX - canvas.offsetLeft
+            if(relativeX > 0 && relativeX < canvas.width) {
+                paddleX = relativeX - paddleWidth/2;
+            }
+        }
+
+
+        this.setPaddleX = (x) => {
+            paddleX = x
+        }
+
+
+        function collisionDetection() {
+            for(var c=0; c<brickColumnCount; c++) {
+                for(var r=0; r<brickRowCount; r++) {
+                    var b = bricks[c][r];
+                    if(b.status === 1) {
+                        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                            dy = -dy;
+
+                            b.status = 0;
+                            score++;
+                            if(score === brickRowCount*brickColumnCount) {
+                                // alert("YOU WIN, CONGRATS!");
+                                // document.location.reload();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        function drawBall() {
+            ctx.beginPath();
+            ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+        function drawPaddle() {
+            ctx.beginPath();
+            ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+        function drawBricks() {
+            for(var c=0; c<brickColumnCount; c++) {
+                for(var r=0; r<brickRowCount; r++) {
+                    if(bricks[c][r].status === 1) {
+                        var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                        var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+                        bricks[c][r].x = brickX;
+                        bricks[c][r].y = brickY;
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "#0095DD";
+                        ctx.fill();
+                        ctx.closePath();
+                    }
+                }
+            }
+        }
+        function drawScore() {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#0095DD";
+            ctx.fillText("Score: "+score, 8, 20);
+        }
+        function drawLives() {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#0095DD";
+            ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+        }
+
+        this.draw = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawBricks();
+            drawBall();
+            drawPaddle();
+            drawScore();
+            drawLives();
+            collisionDetection();
+
+            //bounce left and right side of canvas
+            if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+                dx = -dx;
+                if(this._on_bounce_handler) this._on_bounce_handler()
+            }
+            //bounce top of canvas
+            if(y + dy < ballRadius) {
+                dy = -dy;
+                if(this._on_bounce_handler) this._on_bounce_handler()
+            }
+            else if(y + dy > canvas.height-ballRadius) {
+                //bounce on paddle
+                if(x > paddleX && x < paddleX + paddleWidth) {
+                    dy = -dy;
+                }
+                else {
+                    lives--;
+                    if(!lives) {
+                        // alert("GAME OVER");
+                        // document.location.reload();
+                    }
+                    else {
+                        x = canvas.width/2;
+                        y = canvas.height-30;
+                        dx = 3;
+                        dy = -3;
+                        paddleX = (canvas.width-paddleWidth)/2;
+                    }
+                }
+            }
+
+            if(rightPressed && paddleX < canvas.width-paddleWidth) {
+                paddleX += 7;
+            }
+            else if(leftPressed && paddleX > 0) {
+                paddleX -= 7;
+            }
+
+            x += dx;
+            y += dy;
+        }
+
+    }
+    setupKeyboard() {
+        document.addEventListener("keydown", this.keyDownHandler, false);
+        document.addEventListener("keyup", this.keyUpHandler, false);
+        document.addEventListener("mousemove", this.mouseMoveHandler, false);
+    }
+
+    onBreak(cb) {
+        this._on_break_handler = cb
+    }
+    onBounce(cb) {
+        this._on_bounce_handler = cb
+    }
+}
+
+
