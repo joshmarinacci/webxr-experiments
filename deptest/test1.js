8bfc8bae672b8edb643244195459f2958ff68ac3
Josh Marinacci <joshua@marinacci.org>
2019-07-10 15:13:43 -0700

    more work

--- deptest/test1.js
+++ deptest/test1.js
@@ -1,22 +1,74 @@
-const MakeToc = memoize(function () {
-    base()
-    MakeChap(1)
-    MakeChap(2)
-    log("make toc")
+const fs = require('fs')
+const PATHS = require('path')
+const FSP = fs.promises
+const FSC = fs.constants
+const readdir = fs.promises.readdir
+const rmdir = fs.promises.rmdir
+const unlink = fs.promises.unlink
+const writeFile = fs.promises.writeFile
+
+const MakeToc = memoize(async function () {
+    const dirname = await _base()
+    const filename = PATHS.join(dirname,'toc.html')
+    const ch1 = await MakeChap(1)
+    const ch2 = await MakeChap(2)
+
+    log(`Making the Table of Contents: ${filename}`)
+    const output = `
+        Table of Contents
+        ${ch1.title}
+        ${ch2.title}
+    `
+    return writeFile(filename,output)
 })
 
-const MakeChap = memoize(function(num) {
-    base()
-    log(`making chapter ${num}`)
-    return `made-chap${num}`
+const MakeChap = memoize(async function (num) {
+    const dirname = await _base()
+    const filename = PATHS.join(dirname,`chapter-${num}.html`)
+    log(`Making chapter ${num}: ${filename}`)
+    const body = `The contents of chapter ${num}`
+    return writeFile(filename,body)
+        .then(()=>{
+            return {
+                title:`Chapter ${num}`,
+                body: body
+            }
+        })
 })
 
-const base = memoize(function() {
-    log('making the base')
+// name this target with _base to prevent it from being listed in the usage
+const _base = memoize(async function() {
+    log('Making the base information')
+    const dirname = 'output'
+    return mkdir(dirname)
 })
 
-const tasks = {base, MakeChap, MakeToc}
+const clean = memoize(async function() {
+    log('Cleaning')
+    const dirname = 'output'
+    return readdir(dirname)
+        //delete files in the dir
+        .then(files => {
+            return Promise.all(files.map((filename)=>{
+                return unlink(PATHS.join(dirname,filename))
+            }))
+        })
+        //delete the dir
+        .then(()=> rmdir(dirname))
+})
+
+const tasks = {_base, MakeChap, MakeToc, clean}
+
 // don't need to edit below this line
+
+async function mkdir(dirname) {
+    // access dir
+    return FSP.access(dirname,FSC.W_OK)
+    // if error, then make dir
+        .catch(()=>FSP.mkdir(dirname))
+        // return dir
+        .then(()=>dirname)
+}
 
 function memoize(fun) {
     const memo = new Map()
@@ -36,7 +88,9 @@ function log() {
 
 function printUsage() {
     console.log("node test1.js <taskname>")
-    Object.keys(tasks).forEach((taskName)=>{
+    Object.keys(tasks)
+        .filter(task => task[0] !== '_')
+        .forEach((taskName)=>{
         console.log(`    ${taskName}`)
     })
 }
@@ -46,10 +100,10 @@ function printMissingTask(taskname) {
 }
 
 function runTask(args) {
-    console.log("using args",args)
     const taskName = process.argv[2]
     if (!taskName) return printUsage()
     if (!tasks[taskName]) return printMissingTask(taskName)
     tasks[taskName](...args.slice(1))
 }
+
 runTask(process.argv.slice(2))
