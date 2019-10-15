4ecdc4dd310f8872db81911cdc1b1f3b8b48fa68
joshmarinacci <joshua@marinacci.org>
2019-10-14 17:02:55 -0700

    stub in new logic system

--- hextest/game2d.js
+++ hextest/game2d.js
@@ -1,19 +1,4 @@
-/*
-- component for every hex
-- land -> forest
-- forest -> wood
-- land -> farm
-- farm -> food
-- wood + coins -> building
-- wood + coins + 4 people => building 2
-- 1 adjacent farm => food for 2 people in the city
-- wood + coins + 8 people => building 3
-- people always grow to the limit of their food
-- n-levels to win. each level defined by a map and win conditions.
- */
-
 import {HexMapView, HexSystem} from './hexsystem.js'
-// import {Mouse2DInputSystem} from './mouse2dsystem.js'
 import {CanvasSystem, MouseCanvasInput, HexMapView2D} from "./canvassystem.js"
 import {GameLogicSystem} from './logic.js'
 import {HexMap, generateMap} from './hex.js'
@@ -22,12 +7,13 @@ import {AmbientLight, Clock, Color, DirectionalLight} from "./node_modules/three
 import {World} from "./node_modules/ecsy/build/ecsy.module.js"
 import {pickOneEnumValue} from './common.js'
 import {TERRAINS} from "./globals.js"
+import {LogicSystem} from "./logic2.js"
 
 function setupGame() {
     let world = new World();
     world.registerSystem(CanvasSystem)
     world.registerSystem(HexSystem)
-    // world.registerSystem(GameLogicSystem)
+    world.registerSystem(LogicSystem)
     let game = world.createEntity()
 
     const map = new HexMap()
---
+++ hextest/logic2.js
@@ -0,0 +1,14 @@
+/*
+- component for every hex
+- land -> forest
+- forest -> wood
+- land -> farm
+- farm -> food
+- wood + coins -> building
+- wood + coins + 4 people => building 2
+- 1 adjacent farm => food for 2 people in the city
+- wood + coins + 8 people => building 3
+- people always grow to the limit of their food
+- n-levels to win. each level defined by a map and win conditions.
+
+ */
