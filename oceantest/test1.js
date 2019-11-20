b767291518bbd04c202044c58e035429ddcf32c5
joshmarinacci <joshua@marinacci.org>
2019-11-20 13:48:11 -0800

    add rocks

--- oceantest/test1.js
+++ oceantest/test1.js
@@ -3,12 +3,16 @@ import {AmbientLight, PlaneBufferGeometry, SphereBufferGeometry, Color, Directio
 TextureLoader,
     RepeatWrapping,
     BackSide,
+    Vector3,
     Mesh} from "./node_modules/three/build/three.module.js"
+import {GLTFLoader} from "./node_modules/three/examples/jsm/loaders/GLTFLoader.js"
 import {System, World} from "./node_modules/ecsy/build/ecsy.module.js"
 import {oneWorldTick, startWorldLoop, ThreeCore, ThreeSystem, toRad} from "./threesystem.js"
 
 
-
+function randf(min,max) {
+    return min + Math.random()*(max-min)
+}
 
 
 
@@ -103,13 +107,64 @@ ThreeObjectManager.queries = {
     }
 }
 
+class GLTFModel {
+    constructor() {
+        this.src = null
+        this.position = new Vector3()
+    }
+}
+
+class GLTFModelSystem extends System {
+    execute() {
+        this.queries.three.results.forEach(ent => {
+            const core = ent.getComponent(ThreeCore)
+            this.queries.objs.added.forEach(ent => {
+                const modelComp = ent.getMutableComponent(GLTFModel)
+                new GLTFLoader().load(modelComp.src, (gltf) => {
+                    const obj = gltf.scene.children[0]
+                    core.getStage().add(obj)
+                    obj.position.x = modelComp.position.x
+                    obj.position.z = modelComp.position.z
+                    obj.scale.x = 2.0
+                    obj.scale.y = 2.0
+                    obj.scale.z = 2.0
+                })
+            })
+        })
+    }
+}
+
+GLTFModelSystem.queries = {
+    three: {
+        components: [ThreeCore]
+    },
+    objs: {
+        components: [GLTFModel],
+        listen: {
+            added:true,
+            removed:true
+        }
+    }
+}
+
+
 function setup() {
     let world = new World();
     world.registerSystem(ThreeSystem)
     world.registerSystem(ThreeObjectManager)
+    world.registerSystem(GLTFModelSystem)
 
     let game = world.createEntity()
-    game.addComponent(ThreeCore)
+    game.addComponent(ThreeCore, {debug:true})
+    /* Setting debug to true will move the camera to point down from above and turn on wireframes for all materials
+
+    add some rocks to give a sense of scale. requires GLTF Mesh, which takes a different code path.
+
+    see if there is a way I can add a second texture to the floor
+    create an animated texture type. it loads a strip into a texture and updates the UV values from there.
+    this will probably require a custom shader
+
+     */
 
 
     let ground = world.createEntity()
@@ -118,6 +173,13 @@ function setup() {
     ground.addComponent(TextureMaterial, {
         src:"diffuse_small.png", wrapW:50, wrapH: 50
     })
+
+    for(let i=0; i<5; i++) {
+        let rock = world.createEntity()
+        rock.addComponent(GLTFModel, {
+            position:{x:randf(-5,5),y:0,z:randf(-5,-10)},
+            src:'rock1.glb'})
+    }
 
 
     oneWorldTick(game,world)
