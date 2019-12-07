b83939be74e74d7899669f5941a4fd89e85ae2b2
Josh Marinacci <joshua@marinacci.org>
2019-12-06 16:47:42 -0800

    add fish geometry and wagging behavior to the main demo

--- oceantest/test1.js
+++ oceantest/test1.js
@@ -1,4 +1,6 @@
 import {
+    Shape,
+    ExtrudeBufferGeometry,
     AdditiveBlending,
     Vector3,
     RepeatWrapping,
@@ -22,6 +24,9 @@ import {
     OperatorNode,
     StandardNodeMaterial,
     TextureNode,
+    PositionNode,
+    SwitchNode,
+    JoinNode,
     TimerNode,
     UVTransformNode,
     UVNode,
@@ -42,7 +47,7 @@ import {
     ThreeSystem,
     toRad
 } from "../josh_common_ecsy/index.js"
-import {AmbientLight, Position, Rotation} from '../josh_common_ecsy/ThreeObjectManager.js'
+import {AmbientLight, CustomGeometry, Position, Rotation} from '../josh_common_ecsy/ThreeObjectManager.js'
 import {ParticleEmitter, ParticleSystem} from '../josh_common_ecsy/particles.js'
 import {Waypoint, WaypointFollower, WaypointSystem} from './waypointsystem.js'
 
@@ -59,28 +64,96 @@ function setupLights(game, world) {
     game.addComponent(AmbientLight, {intensity:0.8})
 }
 
+function makeFishGeometry() {
+    let shape = new Shape();
+    let width = 1.0
+    let height = 1.0
+    shape.moveTo( 0,0.75 );
+    shape.lineTo( 0.25, 0.6 );
+    shape.lineTo(0.4,1);
+    shape.lineTo(0.75,0.9)
+    shape.lineTo(1,0.50)
+    shape.lineTo(0.90,0.25)
+    shape.lineTo(0.70,0.18)
+    shape.lineTo(0.6,0)
+    shape.lineTo(0.35,0.1)
+    shape.lineTo(0.25,0.40)
+    shape.lineTo( 0.05, 0.25 );
+    shape.lineTo( 0.10, 0.5 );
+
+
+    let extrudeSettings = {
+        steps:3,
+        depth: 0.05,
+        bevelEnabled: false,
+        bevelThickness: 0.05,
+        bevelSize: 0.1,
+        bevelOffset: -0.1,
+        bevelSegments: 2
+    };
+
+    let geo = new ExtrudeBufferGeometry( shape, extrudeSettings );
+    geo.translate(-1,0,0)
+    geo.scale(1,0.8,1)
+    geo.rotateY(toRad(-90))
+    return geo
+}
+
+function makeFishWagMaterial(fish, world) {
+    const mul = (v1,v2) => new OperatorNode(v1,v2,OperatorNode.MUL)
+    const add = (v1,v2) => new OperatorNode(v1,v2,OperatorNode.ADD)
+    const f = (v1) => new FloatNode(v1)
+    const sin = (v1) => new MathNode(v1,MathNode.SIN)
+
+
+
+    const material = new StandardNodeMaterial();
+    let time = new TimerNode();
+    let speed = f(5.0)
+    time = mul(time,speed)
+    let freq = f(3.0)
+
+    material.color = new ColorNode('green')
+
+    const localPos = new PositionNode()
+
+    const centerOffset = f(0.75)
+    const x = new SwitchNode(localPos,'z')
+    //sin(x * freq + time) * ((x-5)*0.1),
+    const nx = mul(
+        sin(add(mul(x,freq),time)),
+        mul(add(x,centerOffset),f(0.1))
+    )
+
+    let offset = new JoinNode(nx, 0, 0 )
+
+    material.position = add(localPos,offset)
+
+    material.color = new TextureNode(new TextureLoader().load("tangtexture.jpg"))
+    return material
+}
 
 function makeFish(world) {
-    // make fish
+    const geo = makeFishGeometry()
+    const mat = makeFishWagMaterial()
+    // make fishes
     for(let i=0; i<10; i++) {
         const fish2 = world.createEntity()
         fish2.addComponent(ThreeObject)
-        fish2.addComponent(Position, {y: 2, z: randf(-5,-10), x: randf(-10,10)})
-        fish2.addComponent(BoxGeometry, { width: 0.2, height: 0.2, length: 1})
+        fish2.addComponent(CustomGeometry, {geometry:geo})
+        fish2.addComponent(Position, {y: 2, z: randf(0,-5), x: randf(-10,10)})
         fish2.addComponent(FlatColor, {color: 'green'})
-        fish2.addComponent(Rotation, {z: toRad(90)})
+        fish2.addComponent(CustomNodeMaterial, {material:mat})
         fish2.addComponent(WaypointFollower)
     }
 
     // make waypoints
     for(let i=0; i<10; i++) {
         const waypoint1 = world.createEntity()
-        waypoint1.addComponent(ThreeObject)
         waypoint1.addComponent(Position, {
             x: randf(-10,10),
             y: randf(0,5),
-            z: randf(-10,-20)})
-        waypoint1.addComponent(SphereGeometry, {radius: 0.5})
+            z: randf(0,-10)})
         waypoint1.addComponent(Waypoint)
     }
 
